bab208fff618acff87d42d32dc4205192795e241
Vivek Matalia <vivekonfire@pop-os.localdomain>
2021-09-23 20:14:46 +0530

    fixed bugs

--- server.js
+++ server.js
@@ -16,4 +16,8 @@ app.use("/api/memo", require("./routes/memories"));
 
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
 app.listen(PORT, () => console.log(`Server running at ${PORT}`));
