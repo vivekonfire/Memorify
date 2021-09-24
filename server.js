dfc800fdb566e751b5f68275f0409cf38162df95
Vivek Matalia <vivekonfire@pop-os.localdomain>
2021-09-24 14:01:20 +0530

    improved

--- server.js
+++ server.js
@@ -20,8 +20,10 @@ app.use("/api/memo", require("./routes/memories"));
 
 const PORT = process.env.PORT || 5000;
 
+// const __dirname = path.resolve();
+
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname, "/client/build")));
 
     app.get("*", (req, res) =>
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
